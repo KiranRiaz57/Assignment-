cf9549fe1c5febe979ecf77ed28202ae99b79695
KiranRiaz57 <159231693+KiranRiaz57@users.noreply.github.com>
2024-03-10 20:43:15 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//Q2
+// let personName: string = "Kiran";
+// console.log("Hello",personName,"would you like to learn some typescript today?")
+// Q3 
+// lowercase
+// let personName: string = "kiran";
+// console.log(personName.toLowerCase());
+//uppercase
+// console.log(personName.toUpperCase());
+// titlecase
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
